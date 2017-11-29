23cd91d8571f5b7d6c56870c9ee1e1420c550859
Ivan Belitskii <belitskii@gmail.com>
2017-11-29 18:58:04 +0300

    Fix remarks

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,10 +4,10 @@ class Test < ApplicationRecord
   PASSED_WITH_ERRORS = 2
 
   belongs_to :category
-  has_many :user_tests
+  has_many :user_tests, dependent: :destroy
   has_many :users, through: :user_tests
 
   def self.titles_by_category_name(title)
-    joins(:category).where(categories: { title: title }).order(title: :desc)
+    joins(:category).where(categories: { title: title }).order(title: :desc).pluck('title')
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_many :user_tests
+  has_many :user_tests, dependent: :destroy
   has_many :tests, through: :user_tests
 
   def tests_by_difficulty_level(level)
-    Test.joins(:user_tests).where(user_tests: { user_id: id, status: Test::PASSING }, tests: { level: level })
+    Test.joins(:user_tests).where(user_tests: { user_id: id, status: Test::PASSING }).where(level: level)
   end
 end
