6a607217da7e7cb0ab138a2ec4cdb3920063d438
Ivan Belitskii <belitskii@gmail.com>
2017-12-03 15:42:14 +0300

    Add relationships

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,6 +6,8 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :user_tests, dependent: :destroy
   has_many :users, through: :user_tests
+  has_many :questions, dependent: :destroy
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
 
   def self.titles_by_category_name(title)
     joins(:category).where(categories: { title: title }).order(title: :desc).pluck('title')
