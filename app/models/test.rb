221730482e7d26cea4f5fe19470b96a12a01ba0f
Ivan Belitskii <belitskii@gmail.com>
2017-12-05 23:51:07 +0300

    Add scopes Add validation

--- app/models/test.rb
+++ app/models/test.rb
@@ -9,7 +9,15 @@ class Test < ApplicationRecord
   has_many :questions, dependent: :destroy
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
-  def self.titles_by_category_name(title)
+  scope :easy,    -> { where(level: 0..1) }
+  scope :medium,  -> { where(level: 2..4) }
+  scope :hard,    -> { where(level: 5..Float::INFINITY) }
+
+  scope :titles_by_category, lambda { |title|
     joins(:category).where(categories: { title: title }).order(title: :desc).pluck('title')
-  end
+  }
+
+  validates :title, presence: true
+  validates :level, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :name, uniqueness: { scope: :level }
 end
---
+++ db/migrate/20171205203321_add_unique_index_to_tests.rb
@@ -0,0 +1,5 @@
+class AddUniqueIndexToTests < ActiveRecord::Migration[5.1]
+  def change
+    add_index :tests, %i[title level], unique: true
+  end
+end
